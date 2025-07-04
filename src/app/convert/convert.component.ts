1a23e1e2825c6456f2a7b09749bc6c403d4db4ee
Therese Nevander <thne1900@student.miun.se>
2025-07-04 12:32:01 +0200

    Strukturerade upp och ordnade i filerna, la till komponenter och påbörjade arbetsbeskrivning i md-filen.

---
+++ src/app/convert/convert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-convert',
+  imports: [],
+  templateUrl: './convert.component.html',
+  styleUrl: './convert.component.css'
+})
+export class ConvertComponent {
+
+}
