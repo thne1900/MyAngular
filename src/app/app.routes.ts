deaf1a00ffa679e1758dfa773f73ac7c16adebd6
Therese Nevander <thne1900@student.miun.se>
2025-07-06 17:55:34 +0200

    Fyllt ut med komponenter, utformat html, css och ordnat en navigationsmeny.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { StartComponent } from './start/start.component';
+import { ConvertComponent } from './convert/convert.component';
+import { AboutComponent } from './about/about.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: "start", component: StartComponent},
+{path: "convert", component: ConvertComponent},
+{path: "about", component:AboutComponent},
+{path: "", redirectTo: "/start", pathMatch: "full"},
+{path: "404", component: NotfoundComponent},
+{path: "**", component: NotfoundComponent}
+
+];
