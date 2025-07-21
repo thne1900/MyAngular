2a5fb90c62000e95b76e59f311b992a32861e59b
Therese Nevander <thne1900@student.miun.se>
2025-07-21 16:52:02 +0200

    Lagt till en komponent (form) på undersidan "About". Gjorde formuläret statiskt.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FormComponent } from '../form/form.component';
+
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  imports: [],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+  sendMessage(){
+    alert ("Tack! Dina synpunkter har skickats")
+  }
+
+}
